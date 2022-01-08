c283a1429fa74547e1ac617fea468e483d38267f
yar <ahmadyar228@gmail.com>
2022-01-08 14:58:57 +0500

    DRF tutorial part 1-Serialization

--- snippets/models.py
+++ snippets/models.py
@@ -17,5 +17,6 @@ class Snippet(models.Model):
     style = models.CharField(
         choices=STYLE_CHOICES, default='friendly', max_length=100
         )
+    
     class Meta:
         ordering = ['created']
